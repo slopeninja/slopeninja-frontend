211e8c442c329c44b45cdcd2360c642bf8620acb
Julia Qiu <juliaqiuxy@gmail.com>
2017-05-02 15:41:05 -0700

    linkup mappin to router

--- src/components/Main/MapPin.js
+++ src/components/Main/MapPin.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
+import {
+  Link,
+} from 'react-router-dom';
 import mapPin from './ResortInfoCard/images/pin.svg';
 
 class MapPin extends Component{
 
   render() {
     return (
-      <img
-        onClick={()=>{alert(this.props.resort.name)}}
-        alt="Map Pin"
-        src={this.props.resort.logo}
-        style={{
-          position: 'relative',
-          height: '40px',
-          width: '40px',
-          top: -50,
-          left: -3,
-        }}
-      />
+      <Link
+        to={`/resorts/${this.props.resort.id}`}
+      >
+        <img
+          alt="Map Pin"
+          src={this.props.resort.logo}
+          style={{
+            position: 'relative',
+            height: '40px',
+            width: '40px',
+            top: -50,
+            left: -3,
+          }}
+        />
+      </Link>
     );
   }
 }
