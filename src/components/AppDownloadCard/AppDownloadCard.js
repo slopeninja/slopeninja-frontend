4f3ecf4e5c9df932576737bf8ada46e32e302303
Julia Qiu <juliaqiuxy@gmail.com>
2017-11-09 00:58:28 -0800

    update app store links

--- src/components/AppDownloadCard/AppDownloadCard.js
+++ src/components/AppDownloadCard/AppDownloadCard.js
@@ -10,10 +10,10 @@ const AppDownloadCard = () => (
       Download the Slope Ninja app.
     </span>
     <div className="AppDownloadCard-links">
-      <a href="#">
+      <a href="https://itunes.apple.com/us/app/slope-ninja/id1297809634?ls=1&mt=8" target="_blank" rel="noopener noreferrer">
         <img src={appleStore} alt="download from Apple Store" />
       </a>
-      <a href="#">
+      <a href="https://play.google.com/store/apps/details?id=ninja.slope.app" target="_blank" rel="noopener noreferrer">
         <img src={googlePlay} alt="download from Google Play" />
       </a>
     </div>
