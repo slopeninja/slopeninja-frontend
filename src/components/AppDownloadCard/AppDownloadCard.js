2f74da826d6a9d4bb789d89f324b6fc3f0e0002f
Julia Qiu <juliaqiuxy@gmail.com>
2019-01-17 22:40:11 -0800

    add alexa card

--- src/components/AppDownloadCard/AppDownloadCard.js
+++ src/components/AppDownloadCard/AppDownloadCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import appleStore from '../../../public/images/appleStore.svg';
 import googlePlay from '../../../public/images/googlePlay.svg';
+import alexaStore from '../../../public/images/alexaStore.svg';
 
 import './AppDownloadCard.css';
 
@@ -10,12 +11,19 @@ const AppDownloadCard = () => (
       Download the Slope Ninja app.
     </span>
     <div className="AppDownloadCard-links">
-      <a href="https://itunes.apple.com/us/app/slope-ninja/id1297809634?ls=1&mt=8" target="_blank" rel="noopener noreferrer">
-        <img src={appleStore} alt="download from Apple Store" />
-      </a>
-      <a href="https://play.google.com/store/apps/details?id=ninja.slope.app" target="_blank" rel="noopener noreferrer">
-        <img src={googlePlay} alt="download from Google Play" />
-      </a>
+      <div>
+        <a href="https://itunes.apple.com/us/app/slope-ninja/id1297809634?ls=1&mt=8" target="_blank" rel="noopener noreferrer">
+          <img src={appleStore} alt="download from Apple Store" />
+        </a>
+        <a href="https://play.google.com/store/apps/details?id=ninja.slope.app" target="_blank" rel="noopener noreferrer">
+          <img src={googlePlay} alt="download from Google Play" />
+        </a>
+      </div>
+      <div>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <img src={alexaStore} alt="download from Alexa Skills Store" />
+        </a>
+      </div>
     </div>
   </div>
 );
